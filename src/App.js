35afd29a28308ef3741ca0d0a327c9f273b7c732
dipscoder <dipeshjaiswal140@gmail.com>
2020-12-22 16:54:26 +0530

    Add grid to App.js and apply styles to it

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react'
 import Details from './components/details/Details'
 
+import { Grid } from "@material-ui/core"
+import useStyles from "./styles"
 function App() {
+    const classes = useStyles()
+
     return (
-        <div>
-            <Details /> 
-        </div>
+        <Grid className={classes.grid} container spacing={0} alignItems="center" justify="center" style={{ height : '100vh' }} >
+            <Grid item xs={12} sm={4} >
+                <Details />
+            </Grid>
+            <Grid item xs={12} sm={4} >
+                Main
+            </Grid>
+            <Grid item xs={12} sm={4} >
+                <Details />
+            </Grid>
+        </Grid>
     )
 }
 
