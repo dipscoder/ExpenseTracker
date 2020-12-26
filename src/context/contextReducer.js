9ddf561204d04e825be79722eb2c07da9663dc9d
dipscoder <dipeshjaiswal140@gmail.com>
2020-12-26 23:29:37 +0530

    Save Transaction data in local storage

--- src/context/contextReducer.js
+++ src/context/contextReducer.js
@@ -1,17 +1,19 @@
 // Reducer => Its a function that takes in the old state, & an action and returns a new State
 const contextReducer = (state, action) => {
     console.log(action);
-
+    let transactions;
     switch (action.type) {
         case "DELETE_TRANSACTION":
-            return (
-                state.filter(t => t.id !== action.payload)
-            )
+            transactions = state.filter(t => t.id !== action.payload)
+            localStorage.setItem('transactions',JSON.stringify(transactions))
+
+            return transactions
             
         case "ADD_TRANSACTION":
-            return (
-                [action.payload, ...state]
-            )
+            transactions =[action.payload, ...state]
+            localStorage.setItem('transactions',JSON.stringify(transactions))
+
+            return transactions
 
         default:
             return state;
